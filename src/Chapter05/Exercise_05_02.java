6bc23d456f0126336fc15bf30240675b24fc8713
Jakup Sinani <jakup.sinanii1@gmail.com>
2022-11-19 19:03:18 +0100

    Ex 05_02

---
+++ src/Chapter05/Exercise_05_02.java
@@ -0,0 +1,22 @@
+package Chapter05;
+
+import java.util.Scanner;
+
+public class Exercise_05_02 {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+
+        System.out.println("Enter a integer: ");
+        long number = in.nextLong();
+
+        System.out.println("The sum of the digits in "+number+" is "+sumDigit(number));
+    }
+    public static long sumDigit(long n){
+        int sum=0;
+        while (n>0){
+            sum+=n%10;
+            n/=10;
+        }
+        return sum;
+    }
+}
