bb243523b55bc9fc317e04aa2184d0223416d8a8
Jakup Sinani <jakup.sinanii1@gmail.com>
2022-11-19 19:15:07 +0100

    Ex 06_02

---
+++ src/chapter06/Exercise_06_02.java
@@ -0,0 +1,32 @@
+package chapter06;
+
+import java.util.Scanner;
+
+//        (Reverse the numbers entered) Write a program that reads ten integers and displays
+//      them in the reverse of the order in which they were read.
+    
+public class Exercise_06_02 {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+
+        // Prompt the user to enter ten integers
+        System.out.print("Enter ten integers: ");
+
+        // fill the array
+        fill(array);
+
+        // Display integers in reverse order
+        for (int i = array.length - 1; i >= 0; i--)
+            System.out.print(array[i] + " ");
+        System.out.println();
+    }
+
+    /**
+     * Method fill
+     */
+    public static void fill(int[] array) {
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < array.length; i++)
+            array[i] = input.nextInt();
+    }
+}
