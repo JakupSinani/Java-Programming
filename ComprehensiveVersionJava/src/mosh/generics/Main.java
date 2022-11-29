32b7c53f4729c544308f8aa3b1a1dd9c466f57b0
Jakup Sinani <jakup.sinanii1@gmail.com>
2022-11-29 14:00:25 +0100

    mosh

--- ComprehensiveVersionJava/src/mosh/generics/Main.java
+++ ComprehensiveVersionJava/src/mosh/generics/Main.java
@@ -2,9 +2,15 @@ package mosh.generics;
 
 public class Main {
     public static void main(String[] args) {
-        var list = new GenericList<Integer>();
-        list.add(1);
-        ;
-        int number = list.get(0);
+        var user1 = new User(20);
+        var user2 = new User(20);
+
+        if (user1.compareTo(user2) < 0) {
+            System.out.println("user1 < user2");
+        } else if (user1.compareTo(user2) == 0) {
+            System.out.println("user1==user2");
+        } else {
+            System.out.println("user1>user2");
+        }
     }
 }
--- ComprehensiveVersionJava/src/mosh/generics/User.java
+++ ComprehensiveVersionJava/src/mosh/generics/User.java
@@ -1,5 +1,17 @@
 package mosh.generics;
 
-public class User {
+public class User implements Comparable<User> {
 
+    private final int points;
+
+    public User(int points) {
+        this.points = points;
+    }
+
+    @Override
+    public int compareTo(User other) {
+
+        return points - other.points;
+
+    }
 }
