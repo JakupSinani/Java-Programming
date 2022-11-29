8d4ab1c1494dca0c179770d00c809f45df1b5b50
Jakup Sinani <jakup.sinanii1@gmail.com>
2022-11-29 14:01:53 +0100

    mosh

--- ComprehensiveVersionJava/src/mosh/generics/Main.java
+++ ComprehensiveVersionJava/src/mosh/generics/Main.java
@@ -2,6 +2,7 @@ package mosh.generics;
 
 public class Main {
     public static void main(String[] args) {
-       Utils.max()
+        var max = Utils.max(1, 3);
+        System.out.println(max);
     }
 }
