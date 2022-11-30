4d0e7afab76a370c4e85df5913e6d99998f40d8d
Jakup Sinani <jakup.sinanii1@gmail.com>
2022-11-30 16:03:13 +0100

    Generics ArrayList

--- HeadFirstJava/src/chapter16/generics/TestGenerics1.java
+++ HeadFirstJava/src/chapter16/generics/TestGenerics1.java
@@ -1,18 +1,22 @@
 package chapter16.generics;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class TestGenerics1 {
     public static void main(String[] args) {
 
     }
 
     public void go() {
-        Animal[] animals = {new Dog(), new Cat(), new Dog()};
-        Dog[] dogs = {new Dog(), new Dog(), new Dog()};
-        takeAnimals(animals);
-        takeAnimals(dogs);
+        ArrayList<Animal> animalArrayList = new ArrayList<>();
+        animalArrayList.add(new Dog());
+        animalArrayList.add(new Cat());
+        animalArrayList.add(new Dog());
+        takeAnimals(animalArrayList);
     }
 
-    public void takeAnimals(Animal[] animals) {
+    public void takeAnimals(ArrayList<Animal> animals) {
         for (Animal a : animals) {
             a.eat();
         }
