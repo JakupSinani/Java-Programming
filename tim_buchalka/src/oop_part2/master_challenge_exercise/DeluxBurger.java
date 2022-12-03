c2eb5f4e0e86779c8ce23342c4f9acd167e9525b
Jakup Sinani <jakup.sinanii1@gmail.com>
2022-12-03 21:25:23 +0100

    MASTER-CHALLENGE-EXERCISE This solution is not 100% perfect. It would be better to use List.

---
+++ tim_buchalka/src/oop_part2/master_challenge_exercise/DeluxBurger.java
@@ -0,0 +1,29 @@
+package oop_part2.master_challenge_exercise;
+
+public class DeluxBurger extends Hamburger {
+    public DeluxBurger() {
+        super("Delux", "Sausage & Bacon", 14.54, "White");
+        super.addHamburgerAddition1("Chips", 2.75);
+        super.addHamburgerAddition2("Drink", 1.81);
+    }
+
+    @Override
+    public void addHamburgerAddition1(String name, double price) {
+        System.out.println("Cannot not add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        System.out.println("Cannot not add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        System.out.println("Cannot not add additional items to a deluxe burger");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("Cannot not add additional items to a deluxe burger");
+    }
+}
---
+++ tim_buchalka/src/oop_part2/master_challenge_exercise/Main.java
@@ -0,0 +1,24 @@
+package oop_part2.master_challenge_exercise;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        double price = hamburger.itemizeHamburger();
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.12);
+        System.out.println("Total Burger price is " + hamburger.itemizeHamburger());
+
+        HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.67);
+        healthyBurger.addHamburgerAddition1("Egg", 5.43);
+        healthyBurger.addHealthAddition1("Lentils", 3.41);
+        System.out.println("Total Healthy Burger price is " + healthyBurger.itemizeHamburger());
+
+        DeluxBurger deluxBurger = new DeluxBurger();
+        deluxBurger.itemizeHamburger();
+        deluxBurger.addHamburgerAddition1("Should not do this", 50.53);
+        deluxBurger.itemizeHamburger();
+
+    }
+}
