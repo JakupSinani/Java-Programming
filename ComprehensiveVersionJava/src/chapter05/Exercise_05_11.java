29a7729d60f928c44a1ba65d36dfb6e0a2a7bc44
Jakup Sinani <jakup.sinanii1@gmail.com>
2023-01-10 22:34:43 +0100

    .

--- ComprehensiveVersionJava/src/chapter05/Exercise_05_11.java
+++ ComprehensiveVersionJava/src/chapter05/Exercise_05_11.java
@@ -1,10 +1,29 @@
 package chapter05;
 
-import java.util.Scanner;
-
 public class Exercise_05_11 {
-   /* public static double computeCommision(double salesAmount){
+    public static void main(String[] args) {
+        System.out.println("\nSales amount   Commision");
+        for (double salesAmount = 1000; salesAmount <= 100000; salesAmount += 5000) {
+            System.out.printf("%-16.0f", salesAmount);
+            System.out.printf("%8.1f\n", computeCommision(salesAmount));
+        }
+    }
 
+    public static double computeCommision(double salesAmount) {
+        double balance = 0;  //  holds the balance
+        double commision = 0;
 
-    }*/
+        //   if sales amount is $10.000.01 and above commission rate is 12%
+        if (salesAmount >= 10000.01) {
+            commision += (balance = salesAmount - 10000) * 0.12;
+        }
+        if (salesAmount >= 5000.01) {
+            commision += (balance - balance - 5000) * 0.10;
+        }
+        if (salesAmount >= 0.01) {
+            commision = balance * 0.08;
+        }
+        return commision;
+
+    }
 }
